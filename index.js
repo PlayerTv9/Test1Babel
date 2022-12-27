c17a09ac672b55645ff679ee478a9dfcb0bab429
PlayerTv9 <tommaso.ibba09@gmail.com>
2022-12-27 21:56:34 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path'
+const app = express();
+app.use(bodyParser.json());
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname, '/home.html'));
+})
+app.get('/cane',(req,res) => {
+    res.sendFile(path.join(__dirname, '/cane.html'));
+})
+app.listen(5000,() => {
+     console.log(`app is listening to port http://localhost:5000`);
+})
